2cd00ffaf9d046a97c9e27532dab7e02212118d9
matthijsvanderplas <mvdplas84@ziggo.nl>
2023-01-17 22:39:58 +0100

    About section added

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -7,14 +7,30 @@
   function handleClick() {
     open = !open
   }
+  let y;
+  let primary = 'white'
+
+$: if (y >= 100 && y <= 260 ) {
+  primary = 'black'
+} else if (y >= 480 && y <= 630) {
+  primary = 'black'
+} else {
+  primary = 'white'
+}
+
 
 </script>
+
+<svelte:window bind:scrollY={y} />
+
 <nav class="fixed z-20 flex justify-end min-w-full">
-    <Hamburger bind:open --color='white' --active-color="black" />
+    <Hamburger bind:open --color={primary} --active-color='black' class='test' />
 </nav>
 
   <section class="{open ? 'flex' : 'hidden' } fixed z-10 top-[0px] right-0 w-full flex-col items-center nav">
-    <Logo />
+    <div class="relative left-[-40px] pt-2 pb-4">
+      <Logo />
+    </div>
     {#each menuItems as item}
     <a href='#{item}' class='btn btn-ghost text-2xl p-2'><button on:click={handleClick} >{item.charAt(0).toUpperCase() + item.slice(1)}</button></a>
     {/each}
@@ -24,4 +40,5 @@
     .nav {
       background-color: #efefef;
     }
+
   </style>
