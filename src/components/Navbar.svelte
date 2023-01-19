a77265b900e73326b13899a5cc3bbe7b142471b1
matthijsvanderplas <mvdplas84@ziggo.nl>
2023-01-19 21:30:55 +0100

    Semantic and styling changes

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -25,6 +25,7 @@ $: if (y >= 100 && y <= 260 ) {
 
 <nav class="fixed z-20 flex justify-end min-w-full">
     <Hamburger bind:open --color={primary} --active-color='black' class='test' />
+  </nav>
   <section class="{open ? 'flex' : 'hidden' } fixed z-10 top-[0px] right-0 w-full flex-col items-center nav">
     <div class="relative left-[-40px] pt-2 pb-4">
       <Logo />
@@ -33,7 +34,6 @@ $: if (y >= 100 && y <= 260 ) {
     <a href='#{item}' class='btn btn-ghost text-2xl p-2'><button on:click={handleClick} >{item.charAt(0).toUpperCase() + item.slice(1)}</button></a>
     {/each}
    </section>
-</nav>
 
   <style>
     .nav {
