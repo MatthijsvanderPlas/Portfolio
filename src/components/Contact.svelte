59aab19ea2db6f010427f2bb45770ef1b1917b07
matthijsvanderplas <mvdplas84@ziggo.nl>
2023-01-23 12:16:27 +0100

    Fully Responsive

--- src/components/Contact.svelte
+++ src/components/Contact.svelte
@@ -22,16 +22,18 @@
 </script>
 
 <article id="contact" class="flex flex-col justify-center p-4 bg-[#2D3E4F] text-[#efefef] min-h-screen">
-  <h2 class="text-3xl p-4">Contact</h2>
-  <form on:submit|preventDefault={handleSubmit}>
-    <input type="hidden" name="contact_number">
-    <label for="name" name="user_name" class="text-xl">Name:</label>
-    <input type="text" bind:this={name} id="name" name="user_name" class="w-full p-1 px-2 rounded text-xl" required/>
-    <label for="email" name="user_email" class="text-xl">Email:</label>
-    <input type="email" bind:this={email} id="email" name="user_email" class="w-full p-1 px-2 rounded text-xl" required/>
-    <label for="message" name="message" class="text-xl">Message:</label>
-    <textarea id="message" bind:this={message} name="message" class="w-full p-1 px-2 text-xl rounded" rows="6"></textarea>
-    <input type='submit' class="btn text-xl bg-orange-500 text-[#13181d]  w-full">
-  </form>
-  <p class="alert-info text-base rounded my-2 text-center">{confirmation}</p>
+  <div class="lg:max-w-3xl lg:mx-auto">
+    <h2 class="text-3xl  p-4">Contact</h2>
+    <form on:submit|preventDefault={handleSubmit}>
+      <input type="hidden" name="contact_number">
+      <label for="name" name="user_name" class="text-xl">Name:</label>
+      <input type="text" bind:this={name} id="name" name="user_name" class="w-full p-1 px-2 rounded text-xl" required/>
+      <label for="email" name="user_email" class="text-xl">Email:</label>
+      <input type="email" bind:this={email} id="email" name="user_email" class="w-full p-1 px-2 rounded text-xl" required/>
+      <label for="message" name="message" class="text-xl">Message:</label>
+      <textarea id="message" bind:this={message} name="message" class="w-full p-1 px-2 text-xl rounded" rows="6"></textarea>
+      <input type='submit' class="btn text-xl bg-orange-500 text-[#13181d]  w-full">
+    </form>
+    <p class="alert-info text-base rounded my-2 text-center">{confirmation}</p>
+  </div>
   </article>
--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -2,6 +2,7 @@
   import { Hamburger } from 'svelte-hamburgers'
   import Logo from '../../public/logo.svelte'
   let open;
+
   const menuItems = ['about','skills', 'projects','contact']
 
   function handleClick() {
@@ -23,27 +24,41 @@ $: if (y >= 100 && y <= 260 ) {
   primary = 'white'
 }
 
-
+  let width;
+  let bgColor = 'bg-[#efefef]'
 
 </script>
 
-<svelte:window bind:scrollY={y} />
+<svelte:window bind:scrollY={y} bind:innerWidth={width}/>
 
+{#if width < 600}
 <nav class="fixed z-20 flex justify-end min-w-full">
     <Hamburger bind:open --color={primary} --active-color='black' class='test' />
   </nav>
   
-  <section class="{open ? 'flex' : 'hidden' } fixed z-10 top-[0px] right-0 w-full flex-col items-center nav" >
-    <a href="#home"><div class="relative left-[-40px] pt-2 pb-4">
+  <section class="{open ? 'flex' : 'hidden' } fixed z-10 top-0 right-0 w-full flex-col items-center nav" >
+    <a href="#home"><div class="absolute top-0 left-2 pt-2 pb-4">
       <Logo />
     </div></a>
-    <ul class="text-center">
+    <ul class="text-center pt-16">
     {#each menuItems as item, index}
     <li class='text-2xl p-2 w-full font-bold {index === menuItems.length - 1 ? 'pb-4' : ''}'><a href='#{item}'  on:click={handleClick} >{item.charAt(0).toUpperCase() + item.slice(1)}</a></li>
     {/each}
     </ul>
    </section>
 
+{:else}
+   <nav class="fixed top-0 left-0 z-50 min-h-[75px] h-[75px] flex items-center w-full {bgColor}">
+    <div class="max-w-6xl mx-auto flex justify-between items-center">
+    <Logo />
+    <ul class="w-full flex justify-center">
+    {#each menuItems as item}
+ <a href='#{item}' class="inline text-2xl p-2 font-bold"  on:click={handleClick} >{item.charAt(0).toUpperCase() + item.slice(1)}</a>
+    {/each}
+</ul>
+</div>
+   </nav>
+{/if}
   <style>
     .nav {
       background-color: #efefef;
